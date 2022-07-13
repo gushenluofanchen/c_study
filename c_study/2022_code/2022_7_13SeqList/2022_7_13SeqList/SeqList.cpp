3071df8c45cf53fc0ee2885f53d0622ba7474e7c
王瑞 <3170446830@qq.com>
2022-07-13 18:41:52 +0800

    添加2022_7_13SeqList

---
+++ c_study/2022_code/2022_7_13SeqList/2022_7_13SeqList/SeqList.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"SeqList.h"
+
+void SeqListInit(SL* ps, SLDataType x);//≥ı ºªØ
+void SeqListPushBack(SL* ps, SLDataType x);//Œ≤≤Â
+void SeqListPopBack(SL* ps);//Œ≤…æ
+void SeqListPushFront(SL* ps, SLDataType x);//Õ∑≤Â
+void SeqListPopFront(SL* ps);//Õ∑…æ
---
+++ c_study/2022_code/2022_7_13SeqList/2022_7_13SeqList/SeqList.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//#define N 100
+//typedef int SLDataType;
+//
+//
+////静态顺序表
+//typedef struct SeqList {
+//
+//	SLDataType a[N];
+//	int size;//表示数组中有多少数据
+//
+//}SL;
+////静态特点:如果满了就不让插入
+////缺点:给多少空间合适呢,很难确定
+////N给小了不够用,给大了浪费
+//
+//
+////接口函数------命名风格是跟着c++STL库州的,方便后续学习STL
+//void SeqListInit(SL* ps, SLDataType x);//初始化
+//void SeqListPushBack(SL* ps, SLDataType x);//尾插
+//void SeqListPopBack(SL* ps);//尾删
+//void SeqListPushFront(SL* ps, SLDataType x);//头插
+//void SeqListPopFront(SL* ps);//头删
+
+
+
+
+
+//--------------------------------------------------------------------
+
+
+
+
+
+
+typedef int SLDataType;
+
+
+//动态顺序表
+typedef struct SeqList {
+
+	SLDataType* a;
+	int size;//表示数组中有多少数据
+	int capacity;//表示实际能存数据的空间容量
+}SL;
+
+//接口函数------命名风格是跟着c++STL库州的,方便后续学习STL
+void SeqListInit(SL* ps, SLDataType x);//初始化
+void SeqListPushBack(SL* ps, SLDataType x);//尾插
+void SeqListPopBack(SL* ps);//尾删
+void SeqListPushFront(SL* ps, SLDataType x);//头插
+void SeqListPopFront(SL* ps);//头删
